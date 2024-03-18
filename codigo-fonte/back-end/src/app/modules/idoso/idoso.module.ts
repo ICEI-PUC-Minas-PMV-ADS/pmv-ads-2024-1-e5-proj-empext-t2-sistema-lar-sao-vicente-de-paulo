9f4081b80ca3af53ed64ede7f551d2ddadc86326
Gustavo Augusto <gustavo.augusto.f@outlook.com>
2024-03-18 15:17:49 -0300

    feat: Controller de buscar todos os idosos

--- codigo-fonte/back-end/src/app/modules/idoso/idoso.module.ts
+++ codigo-fonte/back-end/src/app/modules/idoso/idoso.module.ts
@@ -3,15 +3,23 @@ import { CreateIdosoService } from "./services/create-idoso.service";
 import { CreateIdosoController } from "./controllers/create-idoso.controller";
 import { PrismaUsuarioRepository } from "@/repositories/prisma/prisma-usuario-repository";
 import { PrismaIdosoRepository } from "@/repositories/prisma/prisma-idoso-repository";
+import { FindAllIdososService } from "./services/find-all-idoso.service";
+import { FindAllIdosoController } from "./controllers/find-all-idoso.controller";
+import { PrismaService } from "@/database/prisma.service";
+import { QueryBuilderService } from "@utils/query-builder/query-builder.service";
 
 @Module({
     providers: [
         CreateIdosoService,
+        FindAllIdososService,
         PrismaIdosoRepository,
         PrismaUsuarioRepository,
+        PrismaService,
+        QueryBuilderService,
     ],
     controllers: [
         CreateIdosoController,
+        FindAllIdosoController,
     ]
 })
 export class IdosoModule { }
