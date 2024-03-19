5394d6f2eb2164327f4d1b7f394f745f0d8db706
Gustavo Augusto <gustavo.augusto.f@outlook.com>
2024-03-19 18:04:45 -0300

    Refactoring: Testes services Idoso

--- codigo-fonte/back-end/src/app/modules/idoso/services/__tests__/create-idoso.service.spec.ts
+++ codigo-fonte/back-end/src/app/modules/idoso/services/__tests__/create-idoso.service.spec.ts
@@ -2,37 +2,55 @@ import { InMemoryIdosoRepository } from "@/repositories/in-memory/in-memory-idos
 import { CreateIdosoService } from "../create-idoso.service";
 import { expect, describe, it, beforeEach } from "vitest";
 import { InMemoryUsuarioRepository } from "@/repositories/in-memory/in-memory-usuario-repository";
-import { IdosoGenero } from "../../dtos/create-idoso.dto";
 import { AppError } from "@utils/app-error";
 
 let idosoRepository: InMemoryIdosoRepository
 let usuarioRepository: InMemoryUsuarioRepository
 let sut: CreateIdosoService
+let existingUsuario
 
 describe("Criação Idoso Caso de Uso", async () => {
     beforeEach(async () => {
         idosoRepository = new InMemoryIdosoRepository()
         usuarioRepository = new InMemoryUsuarioRepository()
         sut = new CreateIdosoService(idosoRepository, usuarioRepository)
-    })
 
-    it("deve criar um novo idoso.", async () => {
-
-        const usuario = await usuarioRepository.create({
+        existingUsuario = await usuarioRepository.create({
             nome: "John Doe",
             email: "johndoe@example.com",
             cpf_cnh: "12345678910",
             senha: "123456",
         });
+    })
+
+    it("deve criar um novo idoso.", async () => {
 
         const idoso = await sut.execute({
-            usuario_id: usuario.uid,
-            nome: "Idoso John Doe",
-            cpf_cnh: "12345678910",
+            usuario_id: existingUsuario.id,
+            nome_completo: "João da Silva",
             data_nascimento: new Date(),
-            data_admissao: new Date(),
-            genero: IdosoGenero.MASCULINO
-
+            naturalidade: "Cidade Fictícia",
+            estado: "Estado Fictício",
+            pais: "País Fictício",
+            estado_civil: "Solteiro(a)",
+            religiao: "Religião Fictícia",
+            escolaridade: "Ensino Médio Completo",
+            nome_pai: "Pai do João",
+            nome_mae: "Mãe do João",
+            data_ingresso: new Date(),
+            cpf: "123.456.789-00",
+            cnh: "123456789",
+            rg: "1234567",
+            rg_orgao_expedidor: "SSP",
+            titulo_eleitor: "987654321",
+            titulo_eleitor_secao: "1234",
+            titulo_eleitor_zona: "5678",
+            certidao_nascimento: "123456789",
+            certidao_nascimento_folha: "567",
+            certidao_nascimento_livro: "123",
+            certidao_casamento: "987654321",
+            certidao_casamento_folha: "321",
+            certidao_casamento_livro: "789",
         })
         expect(idoso.uid).toEqual(expect.any(String));
 
@@ -41,31 +59,61 @@ describe("Criação Idoso Caso de Uso", async () => {
     it("deve lançar um erro ao tentar criar um idoso com CPF existente.", async () => {
         const cpf = "12345678901"
 
-        const usuario = await usuarioRepository.create({
-            nome: "John Doe",
-            email: "johndoe@example.com",
-            cpf_cnh: "12345678910",
-            senha: "123456",
-        });
-
         await sut.execute({
-            usuario_id: usuario.uid,
-            nome: "Idoso John Doe",
-            cpf_cnh: cpf,
+            usuario_id: existingUsuario.id,
+            nome_completo: "João da Silva",
             data_nascimento: new Date(),
-            data_admissao: new Date(),
-            genero: IdosoGenero.MASCULINO
-
+            naturalidade: "Cidade Fictícia",
+            estado: "Estado Fictício",
+            pais: "País Fictício",
+            estado_civil: "Solteiro(a)",
+            religiao: "Religião Fictícia",
+            escolaridade: "Ensino Médio Completo",
+            nome_pai: "Pai do João",
+            nome_mae: "Mãe do João",
+            data_ingresso: new Date(),
+            cpf: cpf,
+            cnh: "123456789",
+            rg: "1234567",
+            rg_orgao_expedidor: "SSP",
+            titulo_eleitor: "987654321",
+            titulo_eleitor_secao: "1234",
+            titulo_eleitor_zona: "5678",
+            certidao_nascimento: "123456789",
+            certidao_nascimento_folha: "567",
+            certidao_nascimento_livro: "123",
+            certidao_casamento: "987654321",
+            certidao_casamento_folha: "321",
+            certidao_casamento_livro: "789",
         })
 
         await expect(() =>
             sut.execute({
-                usuario_id: usuario.uid,
-                nome: "Idoso John Doe 2",
-                cpf_cnh: cpf,
+                usuario_id: existingUsuario.id,
+                nome_completo: "João da Silva",
                 data_nascimento: new Date(),
-                data_admissao: new Date(),
-                genero: IdosoGenero.FEMININO
+                naturalidade: "Cidade Fictícia",
+                estado: "Estado Fictício",
+                pais: "País Fictício",
+                estado_civil: "Solteiro(a)",
+                religiao: "Religião Fictícia",
+                escolaridade: "Ensino Médio Completo",
+                nome_pai: "Pai do João",
+                nome_mae: "Mãe do João",
+                data_ingresso: new Date(),
+                cpf: cpf,
+                cnh: "123456789",
+                rg: "1234567",
+                rg_orgao_expedidor: "SSP",
+                titulo_eleitor: "987654321",
+                titulo_eleitor_secao: "1234",
+                titulo_eleitor_zona: "5678",
+                certidao_nascimento: "123456789",
+                certidao_nascimento_folha: "567",
+                certidao_nascimento_livro: "123",
+                certidao_casamento: "987654321",
+                certidao_casamento_folha: "321",
+                certidao_casamento_livro: "789",
             })
         ).rejects.toThrow(AppError)
     })
--- codigo-fonte/back-end/src/app/modules/idoso/services/__tests__/delete-idoso.service.spec.ts
+++ codigo-fonte/back-end/src/app/modules/idoso/services/__tests__/delete-idoso.service.spec.ts
@@ -1,49 +1,58 @@
 import { InMemoryIdosoRepository } from "@/repositories/in-memory/in-memory-idoso-repository";
 import { expect, describe, it, beforeEach } from "vitest";
 import { InMemoryUsuarioRepository } from "@/repositories/in-memory/in-memory-usuario-repository";
-import { IdosoGenero } from "../../dtos/create-idoso.dto";
 import { DeleteIdosoService } from "../delete-idoso.service";
 import { DeleteIdosoDto } from "../../dtos/delete-idoso.dto";
 import { AppError } from "@utils/app-error";
+import { Situacao } from "@prisma/client";
 
 let idosoRepository: InMemoryIdosoRepository
 let usuarioRepository: InMemoryUsuarioRepository
 let sut: DeleteIdosoService
+let existingIdoso
 
-describe("Criação Idoso Caso de Uso", async () => {
+describe("Remoção Idoso Caso de Uso", async () => {
     beforeEach(async () => {
         idosoRepository = new InMemoryIdosoRepository()
         usuarioRepository = new InMemoryUsuarioRepository()
         sut = new DeleteIdosoService(idosoRepository)
+
+        existingIdoso = await idosoRepository.create({
+            usuario_id: 1,
+            nome_completo: "João da Silva",
+            data_nascimento: "1970-01-01",
+            naturalidade: "Cidade Fictícia",
+            estado: "Estado Fictício",
+            pais: "País Fictício",
+            estado_civil: "Solteiro(a)",
+            religiao: "Religião Fictícia",
+            escolaridade: "Ensino Médio Completo",
+            nome_pai: "Pai do João",
+            nome_mae: "Mãe do João",
+            data_ingresso: "2020-01-01",
+            cpf: "123.456.789-00",
+            cnh: "123456789",
+            rg: "1234567",
+            rg_orgao_expedidor: "SSP",
+            titulo_eleitor: "987654321",
+            titulo_eleitor_secao: "1234",
+            titulo_eleitor_zona: "5678",
+            certidao_nascimento: "123456789",
+            certidao_nascimento_folha: "567",
+            certidao_nascimento_livro: "123",
+            certidao_casamento: "987654321",
+            certidao_casamento_folha: "321",
+            certidao_casamento_livro: "789",
+        })
     })
 
     it("deve excluir o idoso com sucesso.", async () => {
-
-        const usuario = await usuarioRepository.create({
-            id: 1,
-            nome: "John Doe",
-            email: "johndoe@example.com",
-            cpf_cnh: "12345678910",
-            senha: "123456",
-        });
-
-        const idoso = await idosoRepository.create({
-            id: 1,
-            usuario_id: usuario.id,
-            nome: "Idoso John Doe",
-            cpf_cnh: "12345678910",
-            data_nascimento: new Date(),
-            data_admissao: new Date(),
-            genero: IdosoGenero.MASCULINO,
-            situacao: "ATIVO"
-        })
-
         const data: DeleteIdosoDto = {
-            situacao: "INATIVO",
+            situacao: Situacao.INATIVO,
             motivo_inativacao: "TRANSFERIDO",
         };
 
-        const deletedIdoso = await sut.execute(data, idoso.uid)
+        const deletedIdoso = await sut.execute(data, existingIdoso.uid)
 
         expect(deletedIdoso.situacao).toBe("INATIVO")
     })
