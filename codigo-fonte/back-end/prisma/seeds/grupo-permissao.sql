a3f1df7d989e1e53036287c77e4b6e0ce91fb99a
ThiagoTerrinha <thiago.terra13@gmail.com>
2024-03-23 16:06:33 -0300

    add: seeds de permissao

---
+++ codigo-fonte/back-end/prisma/seeds/grupo-permissao.sql
@@ -0,0 +1,11 @@
+INSERT INTO grupo_permissao (id, uid, nome, codigo, atualizado_em) 
+VALUES
+(1, 'fb6acc55-8ef1-4f91-8bbf-5075ad3ff820','Usuario', 100, current_timestamp),
+(2, 'aca3131f-5931-40ac-ae31-7b11fa417ca0','Cargo', 200, current_timestamp),
+(3, 'c843bf96-644a-4cbc-852e-5ab1cb96cc36','Idoso', 300, current_timestamp),
+ON CONFLICT (id) DO UPDATE
+  SET
+    uid = EXCLUDED.uid,
+    nome = EXCLUDED.nome,
+    codigo = EXCLUDED.codigo,
+    atualizado_em = EXCLUDED.atualizado_em;
---
+++ codigo-fonte/back-end/prisma/seeds/permissao.sql
@@ -0,0 +1,21 @@
+INSERT INTO permissao (id, uid, id_grupo_permissao, nome, codigo, atualizado_em) 
+VALUES
+(1, '2df1ad43-dbb4-4dbd-82a2-ccf55cfa97a5',1, 'Criar usuario', 101, current_timestamp),
+(2, '1c658131-9993-4f55-8656-5560d277d703',1, 'Buscar usuario', 102, current_timestamp),
+(3, '038f5969-69d0-4453-98fa-773672c4b85c',1, 'Atualizar usuario', 103, current_timestamp),
+(4, '3cf5e068-3bf1-4003-86bb-b33bcf421fd6',1, 'Deletar usuario', 104, current_timestamp),
+(5, 'de0fa75d-260f-42b5-9134-146055383812',2, 'Cadastrar cargo', 201, current_timestamp),
+(6, '495f9f6c-3de4-4255-8073-8a67b785426e',2, 'Buscar cargo', 202, current_timestamp),
+(7, 'f5924ec8-b950-482d-9003-0f47cb1e94f4',2, 'Atualizar cargo', 203, current_timestamp),
+(8, '6a88d667-6c61-41d9-82cd-4e6f75d25bdd',2, 'Deletar cargo', 204, current_timestamp),
+(9, 'a45c49df-6902-464d-b09d-1d3a86cc96f7',3, 'Cadastrar idoso', 301, current_timestamp),
+(10, 'f20a3e1e-eff4-4f07-b453-cb72cc096aa1',3, 'Buscar idoso', 302, current_timestamp),
+(11, 'c6706f02-7f1a-458b-9f8b-c378fd7aa3e6',3, 'Atualizar idoso', 303, current_timestamp),
+(12, 'c22c6432-9459-43cb-90d2-ae7cb08461d1',3, 'Deletar idoso', 304, current_timestamp),
+ON CONFLICT (id) DO UPDATE
+  SET
+    uid = EXCLUDED.uid,
+    id_grupo_permissao = EXCLUDED.id_grupo_permissao,
+    nome = EXCLUDED.nome,
+    codigo = EXCLUDED.codigo,
+    atualizado_em = EXCLUDED.atualizado_em;
