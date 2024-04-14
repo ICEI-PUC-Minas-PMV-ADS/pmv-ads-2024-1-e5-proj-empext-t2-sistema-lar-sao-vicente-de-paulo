ed1238430452ab581235cc24a4800a9378e1288d
Thiago Terra <thiago.terra13@gmail.com>
2024-04-14 19:12:27 -0300

    add: foto perfil

--- codigo-fonte/front-end/next.config.mjs
+++ codigo-fonte/front-end/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dblardeidosos.blob.core.windows.net",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
