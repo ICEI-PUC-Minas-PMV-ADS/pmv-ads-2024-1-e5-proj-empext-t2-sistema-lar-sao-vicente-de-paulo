81db58704a05b09bc9715c3af8fc689f7200cf2c
Gustavo Augusto <gustavo.augusto.f@outlook.com>
2024-04-18 10:00:01 -0300

    fix: Relatorio Pia Modelo Schema

--- codigo-fonte/back-end/prisma/migrations/20240418125924_add_relatorio_pia/migration.sql
+++ codigo-fonte/back-end/prisma/migrations/20240418125924_add_relatorio_pia/migration.sql
@@ -9,6 +9,9 @@
   - Added the required column `nome` to the `relatorio_pia` table without a default value. This is not possible if the table is not empty.
 
 */
+-- CreateEnum
+CREATE TYPE "typesReposta" AS ENUM ('TEXT', 'RADIO', 'CHECKBOX');
+
 -- DropForeignKey
 ALTER TABLE "pergunta" DROP CONSTRAINT "pergunta_id_modelo_relatorio_pia_fkey";
 
@@ -45,9 +48,23 @@ CREATE TABLE "modelo_relatorio_pia_pergunta" (
 );
 
 -- CreateTable
+CREATE TABLE "modelo_relatorio_pia_resposta" (
+    "id" BIGSERIAL NOT NULL,
+    "uid" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "label" VARCHAR(255) NOT NULL,
+    "type" "typesReposta" NOT NULL,
+    "criado_em" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizado_em" TIMESTAMP NOT NULL,
+    "id_pergunta" BIGINT NOT NULL,
+
+    CONSTRAINT "modelo_relatorio_pia_resposta_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "relatorio_pia_pergunta" (
     "id" BIGSERIAL NOT NULL,
     "uid" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "pergunta" VARCHAR(255) NOT NULL,
     "criado_em" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "atualizado_em" TIMESTAMP NOT NULL,
     "id_pergunta" BIGINT NOT NULL,
@@ -69,8 +86,37 @@ CREATE TABLE "relatorio_pia_resposta" (
     CONSTRAINT "relatorio_pia_resposta_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "relatorio_pia_pergunta_resposta_opcao" (
+    "id" BIGSERIAL NOT NULL,
+    "uid" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "opcao" VARCHAR(255) NOT NULL,
+    "criado_em" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizado_em" TIMESTAMP NOT NULL,
+    "id_resposta" BIGINT NOT NULL,
+
+    CONSTRAINT "relatorio_pia_pergunta_resposta_opcao_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RelatorioPiaPerguntaResposta" (
+    "id" BIGSERIAL NOT NULL,
+    "uid" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "label" VARCHAR(255) NOT NULL,
+    "type" "typesReposta" NOT NULL,
+    "value" VARCHAR(255),
+    "criado_em" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizado_em" TIMESTAMP NOT NULL,
+    "id_respostaOpcao" BIGINT NOT NULL,
+
+    CONSTRAINT "RelatorioPiaPerguntaResposta_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "modelo_relatorio_pia_pergunta_uid_key" ON "modelo_relatorio_pia_pergunta"("uid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "modelo_relatorio_pia_resposta_uid_key" ON "modelo_relatorio_pia_resposta"("uid");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "relatorio_pia_pergunta_uid_key" ON "relatorio_pia_pergunta"("uid");
@@ -81,8 +127,17 @@ CREATE UNIQUE INDEX "relatorio_pia_resposta_uid_key" ON "relatorio_pia_resposta"
 -- CreateIndex
 CREATE UNIQUE INDEX "relatorio_pia_resposta_id_pergunta_key" ON "relatorio_pia_resposta"("id_pergunta");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "relatorio_pia_pergunta_resposta_opcao_uid_key" ON "relatorio_pia_pergunta_resposta_opcao"("uid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RelatorioPiaPerguntaResposta_uid_key" ON "RelatorioPiaPerguntaResposta"("uid");
+
 -- AddForeignKey
 ALTER TABLE "modelo_relatorio_pia_pergunta" ADD CONSTRAINT "modelo_relatorio_pia_pergunta_id_modelo_fkey" FOREIGN KEY ("id_modelo") REFERENCES "modelo_relatorio_pia"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "modelo_relatorio_pia_resposta" ADD CONSTRAINT "modelo_relatorio_pia_resposta_id_pergunta_fkey" FOREIGN KEY ("id_pergunta") REFERENCES "modelo_relatorio_pia_pergunta"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "relatorio_pia" ADD CONSTRAINT "relatorio_pia_id_modelo_fkey" FOREIGN KEY ("id_modelo") REFERENCES "modelo_relatorio_pia"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -95,3 +150,9 @@ ALTER TABLE "relatorio_pia_pergunta" ADD CONSTRAINT "relatorio_pia_pergunta_id_r
 
 -- AddForeignKey
 ALTER TABLE "relatorio_pia_resposta" ADD CONSTRAINT "relatorio_pia_resposta_id_pergunta_fkey" FOREIGN KEY ("id_pergunta") REFERENCES "relatorio_pia_pergunta"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "relatorio_pia_pergunta_resposta_opcao" ADD CONSTRAINT "relatorio_pia_pergunta_resposta_opcao_id_resposta_fkey" FOREIGN KEY ("id_resposta") REFERENCES "modelo_relatorio_pia_resposta"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RelatorioPiaPerguntaResposta" ADD CONSTRAINT "RelatorioPiaPerguntaResposta_id_respostaOpcao_fkey" FOREIGN KEY ("id_respostaOpcao") REFERENCES "relatorio_pia_pergunta_resposta_opcao"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
