615e7a2948b47b4d84d5601041ace09b6ec64e81
Thiago Terra <thiago.terra13@gmail.com>
2024-04-24 19:51:06 -0300

    init

---
+++ codigo-fonte/front-end/src/app/(app)/idoso/Interface/IIdoso.ts
@@ -0,0 +1,36 @@
+import { Situacao } from "@/interface/ISituacao";
+
+export interface IIdoso {
+  id: bigint;
+  uid: string;
+  id_usuario: bigint;
+  foto: string;
+  nome_completo: string;
+  data_nascimento: Date;
+  naturalidade: string;
+  estado: string;
+  pais: string;
+  estado_civil: string;
+  religiao: string;
+  escolaridade: string;
+  nome_pai: string;
+  nome_mae: string;
+  data_ingresso: Date;
+  cpf: string;
+  cnh: string;
+  rg: string;
+  rg_orgao_expedidor: string;
+  titulo_eleitor: string;
+  titulo_eleitor_secao: string;
+  titulo_eleitor_zona: string;
+  certidao_nascimento: string;
+  certidao_nascimento_folha: string;
+  certidao_nascimento_livro: string;
+  certidao_casamento: string;
+  certidao_casamento_folha: string;
+  certidao_casamento_livro: string;
+  situacao: Situacao;
+  motivo_inativacao: string;
+  criado_em: Date;
+  atualizado_em: Date;
+}
