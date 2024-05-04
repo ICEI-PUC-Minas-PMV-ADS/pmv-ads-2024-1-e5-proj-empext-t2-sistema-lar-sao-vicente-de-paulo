a07c68d71db2b966f545487d58abc653f2251af0
Thiago Terra <thiago.terra13@gmail.com>
2024-05-04 13:16:08 -0300

    fix: retirando pattern inputform

--- codigo-fonte/front-end/src/components/input/InputForm.tsx
+++ codigo-fonte/front-end/src/components/input/InputForm.tsx
@@ -64,7 +64,6 @@ export const InputForm = ({
         type={type}
         autoComplete={autoComplete}
         maxLength={maxLength || 100}
-        pattern={"^[a-zA-Z0-9]+$"}
       />
       {error && (
         <div className="flex gap-2 items-center text-red-600 text-xs">
