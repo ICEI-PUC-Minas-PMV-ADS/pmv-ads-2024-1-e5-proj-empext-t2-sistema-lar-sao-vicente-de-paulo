cf7021ab49be6f858f48a8b17a1b252e625f9c69
marianotbc <mariano.contin@cloudtarget.com.br>
2024-06-04 21:17:27 -0300

    add relatorio pia page.

---
+++ codigo-fonte/front-end/src/app/(app)/relatorio-pia/Interface/IRelatorioPia.ts
@@ -0,0 +1,10 @@
+export interface IRelatorioPia {
+  id: bigint;
+  uid: string;
+  nome: string;
+  criado_em: Date;
+  atualizado_em: Date;
+  id_modelo_relatorio_pia: bigint;
+  id_usuario: bigint;
+  id_idoso: bigint;
+}
